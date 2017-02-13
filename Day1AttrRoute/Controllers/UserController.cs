8fdd4c0e9c3e460052fa66efae488d048b338af1
austincrisp <austi@AUSTINBLADE>
2017-02-13 12:06:58 -0500

    URL shortening/cryptography

---
+++ Day1AttrRoute/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Day1AttrRoute.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day1AttrRoute.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: User
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var userInstance = db.Users.Where(u => u.Id == userId).FirstOrDefault();
+            return View(userInstance);
+        }
+
+        [Route("User/{username}")]
+        public ActionResult Detail(string userName)
+        {
+            var userInstance = db.Users.Where(u => u.UserName == userName).FirstOrDefault();
+            return View(userInstance);
+        }
+    }
+}
---
+++ Day1Hash/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Hash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Step 1 - Take user input
+            // Step 2 - Encrypt user input
+
+            string myInput = Console.ReadLine();
+
+            byte[] byteData = Encoding.UTF8.GetBytes(myInput);
+            Stream inputStream = new MemoryStream(byteData);
+
+            using (SHA256 shaM = new SHA256Managed())
+            {
+                var result = shaM.ComputeHash(inputStream);
+                string output = BitConverter.ToString(result);
+                Console.WriteLine(output.Replace("-", "").Substring(0, 5));
+            }
+        }
+    }
+}
