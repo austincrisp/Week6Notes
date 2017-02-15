5e4a575f0711d7d3b9faebb65065f0099db2d9b2
austincrisp <austi@AUSTINBLADE>
2017-02-15 11:09:31 -0500

    added rest of program

--- Day1Hash/Program.cs
+++ Day1Hash/Program.cs
@@ -15,16 +15,11 @@ namespace Day1Hash
             // Step 1 - Take user input
             // Step 2 - Encrypt user input
 
-            string myInput = Console.ReadLine();
-
-            byte[] byteData = Encoding.UTF8.GetBytes(myInput);
-            Stream inputStream = new MemoryStream(byteData);
-
-            using (SHA256 shaM = new SHA256Managed())
+            while (true)
             {
-                var result = shaM.ComputeHash(inputStream);
-                string output = BitConverter.ToString(result);
-                Console.WriteLine(output.Replace("-", "").Substring(0, 5));
+                var myInput = Console.ReadLine();
+                string output = Encryptor.MakeSha256(myInput);
+                Console.WriteLine(output);
             }
         }
     }
